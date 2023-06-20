db08bced9c9a84ecf6fe102e6764029b824086ba
Eduardo Franco <eduardofranco@Eduardos-MacBook-Pro.local>
2023-06-20 19:29:56 +0100

    input text added for searching posts

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -5,13 +5,15 @@ import './styles.css';
 import { loadPosts } from '../../utils/load-posts'
 import { Posts } from '../../components/Posts'
 import { Button } from '../../components/Button'
+import { TextInput } from "../../components/TextInput";
 
 export class Home extends Component {
   state = {
     posts: [],
     allPosts: [],
     page: 0,
-    postsPerPage: 20,
+    postsPerPage: 2,
+    searchValue: '',
   };
 
   componentDidMount() {
@@ -41,19 +43,46 @@ export class Home extends Component {
     this.setState({posts, page: nextPage});
   }
 
+  handleChange = (e) => {
+    const {value} = e.target;
+    this.setState({searchValue: value});
+  }
+
   render() {
-    const { posts, page, postsPerPage, allPosts } = this.state;
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
     const noMorePosts = page + postsPerPage >= allPosts.length;
+
+    const filteredPosts = !!searchValue ?
+      allPosts.filter(post => {
+        return post.title.toLowerCase().includes(
+          searchValue.toLowerCase()
+        )
+    })
+    : posts;
 
     return (
       <section className='container'>
-        <Posts posts={posts} />
+        <div class='search-container'>
+          {!!searchValue && (
+            <>
+              <h1>Search value: {searchValue}</h1>
+            </>
+          )}
+          <TextInput searchValue={searchValue} handleChange={this.handleChange} />
+        </div>
+        
+        {filteredPosts.length > 0 && (
+          <Posts posts={filteredPosts} />
+        )}
+
         <div className='button-container'>
-          <Button
-            text='Load more Posts'
-            onClick={this.loadMorePosts}
-            disabled={noMorePosts}
-          />
+          {!searchValue && (
+            <Button
+              text='Load more Posts'
+              onClick={this.loadMorePosts}
+              disabled={noMorePosts}
+            />
+          )}
         </div>
       </section>
     );
